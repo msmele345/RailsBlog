8e51dc50e55f8df4bfc2ebc7e5f90de3b6e3ca81
Mitch Mele <mitchmele@gmail.com>
2017-11-18 15:53:53 -0600

    Add comment forms and delete routes.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
 end
@@ -7,3 +7,6 @@ end
 #old form header
 ##rails is smart enough to automatically figure out what form we are using it for (new,edit). It actually replaces submit with update if it tells its an edit form
 # <%= form_for :post, url: posts_path do |f| %>
+
+##depedent destroy deletes all comments of a post if a post is deleted
+
